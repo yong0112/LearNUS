d057f2f1b73bbfc168b7b797d0a65cfde2308192
yong1611 <yonglzy1611@gmail.com>
2025-05-30 01:38:54 +0800

    test

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,9 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
 ]);
