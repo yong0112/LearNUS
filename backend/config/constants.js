f29a4ecce2a1dcb17df724f8df29289cfeef3b3e
Jayyy25 <jayt040225@gmail.com>
2025-06-15 21:04:29 +0800

    Add constants for daysInWeek

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -4,4 +4,13 @@ module.exports = {
     { label: "Physical", value: "physical" },
     { label: "Hybrid", value: "hybrid" },
   ],
+  DAYS: [
+    {label: "Monday", value: "mon"},
+    {label: "Tuesday", value: "tue"},
+    {label: "Wednesday", value: "wed"},
+    {label: "Thursday", value: "thu"},
+    {label: "Friday", value: "fri"},
+    {label: "Saturday", value: "sat"},
+    {label: "Sunday", value: "sun"}
+  ]
 };
