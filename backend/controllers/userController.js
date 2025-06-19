522ab744b69cf61f45932a81486807383314c91b
Jayyy25 <jayt040225@gmail.com>
2025-06-19 22:18:09 +0800

    Backend for update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const { getUserProfile } = require("../models/userModel");
+const { getUserProfile, updateUserProfile } = require("../models/userModel");
 
 const fetchUserProfile = async (req, res) => {
   const uid = req.params.uid;
@@ -11,4 +11,23 @@ const fetchUserProfile = async (req, res) => {
   }
 };
 
-module.exports = { fetchUserProfile };
+const updateUser = async (req, res) => {
+  const { uid, newEmail } = req.body();
+
+  if (!uid) return res.status(404).json({error: "UID is required"});
+
+  try {
+    const updateData = {
+      ...(newEmail && { newEmail }),
+      updatedAt: new Date()
+    }
+
+    await updateUserProfile(uid, updateData);
+
+    res.status(200).json({ message: "User profile updates successfully "});
+  } catch (err) {
+    res.status(500).json({ error: "Failed to update user profile", details: err.message });
+  }
+}
+
+module.exports = { fetchUserProfile, updateUser };
