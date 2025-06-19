8677930ca1bd471bcf9dc0d2190021a977160b15
Jayyy25 <jayt040225@gmail.com>
2025-06-19 22:19:10 +0800

    Code style check

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { fetchUserProfile, updateUser } = require("../controllers/userController");
+const {
+  fetchUserProfile,
+  updateUser,
+} = require("../controllers/userController");
 
 router.get("/:uid", fetchUserProfile);
 router.post("/update-profile", updateUser);
