bc86baf2bc18e6cab980bf4e68b4549ea5a7c8bb
Jayyy25 <jayt040225@gmail.com>
2025-07-21 22:31:43 +0800

    Update payment proof

--- backend/routes/session.js
+++ backend/routes/session.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   fetchUserSession,
   updateSessionStatus,
+  updatePaymentProof,
 } = require("../controllers/sessionController");
 
 router.get("/:uid/classes/:cid", fetchUserSession);
-router.post("/:uid/classes/:cid", updateSessionStatus);
+router.post("/:uid/classes/:cid/update-status", updateSessionStatus);
+router.post("/:uid/classes/:cid/update-payment", updatePaymentProof);
 
 module.exports = router;
