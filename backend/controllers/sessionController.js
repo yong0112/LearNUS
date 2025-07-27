5d43b3b3f43a3ab5024c182be56ad966b6939878
Jayyy25 <jayt040225@gmail.com>
2025-07-28 03:19:05 +0800

    Use cron to update booking status automatically every 5 minutes

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -1,6 +1,8 @@
 const {
   getUserSession,
   updateUserSessionField,
+  updateExpiredSessions,
+  getUserSessionWithExpirationCheck,
 } = require("../models/sessionModel");
 const { getUserClasses } = require("../models/classesModel");
 const { sendBookingNotification } = require("../utils/notification");
@@ -82,4 +84,46 @@ const updatePaymentProof = async (req, res) => {
   }
 };
 
-module.exports = { fetchUserSession, updateSessionStatus, updatePaymentProof };
+const cleanupExpiredSessions = async (req, res) => {
+  try {
+    const result = await updateExpiredSessions();
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    console.error("Cleanup controller error:", error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Enhanced version of your fetchUserSession with expiration check
+const fetchUserSessionWithExpiration = async (req, res) => {
+  const uid = req.params.uid;
+  const cid = req.params.cid;
+
+  if (!uid || !cid) {
+    return res
+      .status(400)
+      .json({ error: "Missing uid or cid in the request parameters." });
+  }
+
+  try {
+    const session = await getUserSessionWithExpirationCheck(uid, cid);
+    res.json(session);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  fetchUserSession,
+  updateSessionStatus,
+  updatePaymentProof,
+  cleanupExpiredSessions,
+  fetchUserSessionWithExpiration,
+};
--- backend/routes/session.js
+++ backend/routes/session.js
@@ -4,10 +4,14 @@ const {
   fetchUserSession,
   updateSessionStatus,
   updatePaymentProof,
+  cleanupExpiredSessions,
+  fetchUserSessionWithExpiration,
 } = require("../controllers/sessionController");
 
 router.get("/:uid/classes/:cid", fetchUserSession);
 router.post("/:uid/classes/:cid/update-status", updateSessionStatus);
 router.post("/:uid/classes/:cid/update-payment", updatePaymentProof);
+router.post("/cleanup", cleanupExpiredSessions);
+router.get("/safe/:uid/:cid", fetchUserSessionWithExpiration);
 
 module.exports = router;
